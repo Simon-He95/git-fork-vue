4f809a361f76257d966267b45fd3f0485a001d7f
Simon-He95 <674949287@qq.com>
2022-10-01 11:37:39 +0800

    chore: update

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from 'virtual:generated-pages'
 // import gitFork from '@simon_he/git-fork'
-import { gitForkInVue } from '../../src'
+import gitForkInVue from '../../src'
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
